0d147067eee2e3ac83ee9437cedd53db964f36dd
Athindran <rameshkumarathindran@gmail.com>
2025-04-26 21:41:15 -0400

    get gradients wrt observation from pipeline state

--- brax_utils/wrapped_env/wrapper_env.py
+++ brax_utils/wrapped_env/wrapper_env.py
@@ -16,20 +16,25 @@ class WrappedBraxEnv(ABC):
         return self.env.step(state, action)
     
     @partial(jax.jit, static_argnames='self')
-    def get_trimmed_state(self, state) -> jax.Array:
+    def get_generalized_coordinates(self, state) -> jax.Array:
         return jnp.concatenate([state.pipeline_state.q, state.pipeline_state.qd], axis=-1) 
 
     @partial(jax.jit, static_argnames=['self'])
-    def step_obs(self, state, action):
+    def step_generalized_coordinates(self, state, action):
         new_state = self.env.step(state, action)
-        new_trimmed_state = self.get_trimmed_state(new_state)
-        return new_trimmed_state
+        new_generalized_coordinates = self.get_generalized_coordinates(new_state)
+        return new_generalized_coordinates
     
     @partial(jax.jit, static_argnames=['self'])
-    def get_trimmed_state_grad(self, state, action):
-        state_grad = jax.jacobian(self.step_obs, argnums=0)(state, action).pipeline_state
-        actor_grad = jax.jacobian(self.step_obs, argnums=1)(state, action)
-        return jnp.concatenate([state_grad.q, state_grad.qd], axis=-1), actor_grad
+    def get_generalized_coordinates_grad(self, state, action):
+        state_grad = jax.jacobian(self.step_generalized_coordinates, argnums=0)(state, action).pipeline_state
+        action_grad = jax.jacobian(self.step_generalized_coordinates, argnums=1)(state, action)
+        return jnp.concatenate([state_grad.q, state_grad.qd], axis=-1), action_grad
+
+    @partial(jax.jit, static_argnames=['self'])
+    def get_obs_grad(self, pipeline_state):
+        state_grad = jax.jacobian(self._get_obs, argnums=0)(pipeline_state)
+        return jnp.concatenate([state_grad.q, state_grad.qd], axis=-1)
 
     @partial(jax.jit, static_argnames='self')    
     def reset(self, rng) -> jax.Array:
--- test_brax_env.py
+++ test_brax_env.py
@@ -9,6 +9,15 @@ from brax.io import model
 from brax import envs
 from brax_utils import WrappedBraxEnv, LinearPolicy, ReacherRegularizedGoalCost
 
+# RL trained policy
+def get_neural_policy():
+    make_inference_fn, params, _ = train_fn(environment=brax_env.env)
+    eval_env = envs.create(env_name=env_name, backend=backend)
+    params = model.load_params(f'./brax_utils/trained_models/{env_name}_params')
+    inference_fn = make_inference_fn(params)
+    jit_inference_fn = jax.jit(inference_fn)
+    return jit_inference_fn
+
 sys.path.append(".")
 
 env_name = 'reacher'  # @param ['ant', 'halfcheetah', 'hopper', 'humanoid', 'humanoidstandup', 'inverted_pendulum', 'inverted_double_pendulum', 'pusher', 'reacher', 'walker2d']
@@ -22,21 +31,17 @@ train_fn = {
   'reacher': functools.partial(ppo.train, num_timesteps=1, num_evals=1, reward_scaling=5, episode_length=1000, normalize_observations=True, action_repeat=4, unroll_length=50, num_minibatches=1, num_updates_per_batch=8, discounting=0.95, learning_rate=3e-4, entropy_cost=1e-3, num_envs=1, batch_size=256, max_devices_per_host=8, seed=1),
 }[env_name]
 
-make_inference_fn, params, _ = train_fn(environment=brax_env.env)
-
-eval_env = envs.create(env_name=env_name, backend=backend)
-params = model.load_params(f'./brax_utils/trained_models/{env_name}_params')
-inference_fn = make_inference_fn(params)
-jit_inference_fn = jax.jit(inference_fn)
+policy = get_neural_policy()
 
 rollout = []
 for _ in range(500):
   rollout.append(state.pipeline_state)
   act_rng, rng = jax.random.split(rng)
-  act, _ = jit_inference_fn(state.obs, act_rng)
+  act, _ = policy(state.obs, act_rng)
   state = brax_env.step(state, act)
-  state_grad, action_grad = brax_env.get_trimmed_state_grad(state, act)
-  print(state_grad, action_grad)
+  gc_from_state_grad, gc_from_action_grad = brax_env.get_generalized_coordinates_grad(state, act)
+  obs_from_gc_grad = brax_env.get_obs_grad(state.pipeline_state)
+  #print(gc_from_state_grad, obs_from_gc_grad, gc_from_action_grad)
 
 render_every = 2
 media.write_video(f'./brax_videos/reacher_linear_policy.mp4',
