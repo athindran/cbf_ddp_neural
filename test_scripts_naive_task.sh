21ae79a0bee917eb30eed150be8fc4cdc0f38136
Athindran <rameshkumarathindran@gmail.com>
2025-06-27 23:11:29 -0400

    update description

--- test_scripts_naive_task.sh
+++ test_scripts_naive_task.sh
@@ -1,4 +1,7 @@
-# Paper scripts
+# Test scripts
+
+# Run each config independently for best performance in process time.
+
 python evaluate_soft_ddpcbf.py -cf ./test_configs/reachavoid/test_config_cbf_reachavoid_circle_config_multiple_obs_1_bic5D.yaml -rb 3.0 --naive_task  -ls 'baseline'
 python evaluate_soft_ddpcbf.py -cf ./test_configs/reachability/test_config_cbf_reachability_circle_config_multiple_obs_1_bic5D.yaml -rb 3.0 --naive_task  -ls 'baseline'
 python evaluate_soft_ddpcbf.py -cf ./test_configs/reachavoid/test_config_cbf_reachavoid_circle_config_multiple_obs_2_bic5D.yaml -rb 3.5 --naive_task  -ls 'baseline'
