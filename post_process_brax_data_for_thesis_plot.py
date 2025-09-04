440be8d9953777e186c44b891f5234d590d62d17
Athindran <rameshkumarathindran@gmail.com>
2025-09-04 03:21:04 -0400

    print PDF

--- post_process_brax_data_for_thesis_plot.py
+++ post_process_brax_data_for_thesis_plot.py
@@ -118,7 +118,7 @@ def make_barkour_reachability_plot():
     ax.set_ylabel('Filter time $(s)$')
     ax.set_xlabel('Time (s)')
 
-    plt.savefig('./plots_summary/barkour_summary.png', bbox_inches='tight', dpi=500)
+    plt.savefig('./plots_summary/barkour_summary.pdf', bbox_inches='tight', dpi=500)
 
 
 def make_reacher_plot():
@@ -244,7 +244,7 @@ def make_reacher_plot():
     col1_axes[1].set_ylim([-0.3, 0.3])
     col1_axes[0].legend(fontsize=9, loc='upper left', bbox_to_anchor=(-0.05, 1.48), ncol=1, framealpha=0)
 
-    plt.savefig('./plots_summary/reacher_summary.png', bbox_inches='tight', dpi=500)
+    plt.savefig('./plots_summary/reacher_summary.pdf', bbox_inches='tight', dpi=500)
 
 make_reacher_plot()
 make_barkour_reachability_plot()
