b2f479d10bc551f3d8c8f104dbff727aa397b1e6
Athindran <rameshkumarathindran@gmail.com>
2025-09-04 02:39:50 -0400

    update script

--- test_scripts_ilqr_task.sh
+++ test_scripts_ilqr_task.sh
@@ -9,7 +9,7 @@ python evaluate_soft_ddpcbf.py -cf ./test_configs/reachability/test_config_cbf_r
 python evaluate_soft_ddpcbf.py -cf ./test_configs/reachability/test_config_cbf_reachability_circle_config_multiple_obs_2_bic4D.yaml -rb 3.5 -ls 'baseline'
 python evaluate_soft_ddpcbf.py -cf ./test_configs/reachability/test_config_cbf_reachability_circle_config_multiple_obs_2_bic5D.yaml -rb 3.5 -ls 'baseline'
 python evaluate_soft_ddpcbf.py -cf ./test_configs/reachability/test_config_cbf_reachability_circle_config_shorter_wheelbase_multiple_obs_1_bic4D.yaml -rb 2.5 -ls 'baseline'
-python evaluate_soft_ddpcbf.py -cf ./test_configs/reachability/test_config_cbf_reachability_circle_config_shorter_wheelbase_multiple_obs_1_bic5D.yaml -rb 2.5 -ls 'baseline'
+python evaluate_soft_ddpcbf.py -cf ./test_configs/reachability/test_config_cbf_reachability_circle_config_shorter_wheelbase_multiple_obs_1_bic5D.yaml -rb 3.5 -ls 'baseline'
 
 # # Reachavoid
 python evaluate_soft_ddpcbf.py -cf ./test_configs/reachavoid/test_config_cbf_reachavoid_circle_config_multiple_obs_1_bic4D.yaml -rb 2.5 -ls 'baseline'
